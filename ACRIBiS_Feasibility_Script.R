55357aef89d4c623657ab5ededcceffe0d36408a
matthiasgietzelt <50948239+matthiasgietzelt@users.noreply.github.com>
2024-09-20 16:38:14 +0200

    Update ACRIBiS_Feasibility_Script.R
    added ", collapse = ","" to concat subject ids at calling body_conditions <- fhir_body()

--- ACRIBiS_Feasibility_Script.R
+++ ACRIBiS_Feasibility_Script.R
@@ -190,7 +190,7 @@ write(paste(length(bundles_patient), " Bundles for the Patient-Ressource were fo
 #Condition
 #now load all CONDITIONS for relevant patient IDs, to obtain other conditions (comorbidities) of relevant patients
 #use "patient" as FHIR-search parameter in Condition resource
-body_conditions <- fhir_body(content = list("subject" = paste(patient_ids_with_conditions)))
+body_conditions <- fhir_body(content = list("subject" = paste(patient_ids_with_conditions, collapse = ",")))
 request_conditions <- fhir_url(url = diz_url, resource = "Condition")
 #code or normcode?; normcode appears to work
 bundles_condition <- fhir_search(request = request_conditions, body = body_conditions, max_bundles = bundle_limit, username = username, password = password)
